3c4d59719410ab82742f439c30659d7668fc70a1
Rishabh Sharma <rish6696.rs@gmail.com>
2019-07-19 15:31:30 +0530

    intented

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
-const express=require('express');
-const app=express();
-const ejs=require('ejs')
-const paypal=require('paypal-rest-sdk');
-const dotenv=require('dotenv');
+const express = require('express');
+const app = express();
+const ejs = require('ejs')
+const paypal = require('paypal-rest-sdk');
+const dotenv = require('dotenv');
 dotenv.config();
 
 
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({
+    extended: true
+}))
 app.use(express.json())
 
-app.set('view engine','ejs')
-app.get('/',(req,res)=>{
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => {
     res.render('index')
 })
 paypal.configure({
-    mode:"sandbox",
-    client_id:process.env.CLIENT_ID,
-    client_secret:process.env.CLIENT_SECRET
+    mode: "sandbox",
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET
 })
-app.post('/pay',(req,res)=>{
+app.post('/pay', (req, res) => {
     const create_payment_json = {
         "intent": "sale",
         "payer": {
@@ -49,20 +51,23 @@ app.post('/pay',(req,res)=>{
         if (error) {
             res.send(error)
         } else {
-            res.redirect(getRedirectUrl(payment.links).href) 
+            res.redirect(getRedirectUrl(payment.links).href)
         }
     });
 })
 
-function getRedirectUrl(array){
-    return array.find((ele)=>{
-        return ele.rel==='approval_url'
+function getRedirectUrl(array) {
+    return array.find((ele) => {
+        return ele.rel === 'approval_url'
     })
 }
 
-app.get('/success',(req,res)=>{
+app.get('/success', (req, res) => {
 
-    const {paymentId,PayerID}=req.query
+    const {
+        paymentId,
+        PayerID
+    } = req.query
     const execute_payment_json = {
         "payer_id": PayerID,
         "transactions": [{
@@ -71,27 +76,26 @@ app.get('/success',(req,res)=>{
                 "total": "25.00"
             }
         }]
-      };
+    };
 
-      paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
+    paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
         if (error) {
             console.log(error.response);
             throw error;
         } else {
-            
+
             res.send(JSON.stringify(payment));
         }
     });
 })
 
-//zsff
-app.get('/cancel',(req,res)=>{
+app.get('/cancel', (req, res) => {
     res.send('cancel')
 })
 
 
 
 
-app.listen(port=1210,()=>{
-    console.log('server started at '+port)
+app.listen(port = 1210, () => {
+    console.log('server started at ' + port)
 })
